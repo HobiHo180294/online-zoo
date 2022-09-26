d6cfde55fc1a8feaab8e50981e4ee4fe955ad837
Yehor Potebenko <hitechnic.uu68@gmail.com>
2022-09-26 13:59:16 +0300

    feat: add pets block

--- online-zoo/src/pages/main/script.mjs
+++ online-zoo/src/pages/main/script.mjs
@@ -1,1 +1,13 @@
 import "@main/style.scss";
+
+const petImageWrapper = document.querySelectorAll(".card-pets__img");
+const petDescription = document.querySelectorAll(".desc-pets");
+
+petImageWrapper.forEach((element) =>
+  element.addEventListener("mouseover", (e) => {
+    element.nextElementSibling.classList.add("_active");
+  })
+);
+
+// petImage.addEventListener("mouseover", (e) => {
+// });
