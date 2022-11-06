72dc6d015f9fd6b6fc56bed3b1a215279cfc10e3
Yehor Potebenko <hitechnic.uu68@gmail.com>
2022-11-06 16:51:08 +0100

    feat: add 'Watch your favourite animal online' section

--- online-zoo/src/pages/donate/script.mjs
+++ online-zoo/src/pages/donate/script.mjs
@@ -8,3 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function ibg() {
+  let ibg = document.querySelectorAll("._ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector("img")) {
+      ibg[i].style.backgroundImage =
+        "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
+    }
+  }
+}
+
+ibg();
